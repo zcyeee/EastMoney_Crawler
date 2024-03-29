573aa272bc08317120f6efc9f6f557c2ad094c82
Chenyang Zhang <61002783+zcyeee@users.noreply.github.com>
2024-03-29 15:35:51 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -25,13 +25,14 @@ if __name__ == "__main__":
     start_time = time.time()
 
     # 爬取发帖信息
-    thread1 = threading.Thread(target=post_thread, args=('000799', 5))  # 设置想要爬取的股票代码和页数
-    thread2 = threading.Thread(target=post_thread, args=('000729', 5))  # 可同时进行多个线程
+    thread1 = threading.Thread(target=post_thread, args=('000333', 500))  # 设置想要爬取的股票代码和页数
+    thread2 = threading.Thread(target=post_thread, args=('000729', 500))  # 可同时进行多个线程
 
     # 爬取评论信息，注意需先爬取发帖信息储存到数据库里后才可以爬取评论信息（因为需要用到第一步中的url）
-    # thread1 = threading.Thread(target=comment_thread_date, args=('000651', '2020-01-01', '2023-12-31'))
-    # thread2 = threading.Thread(target=comment_thread_date, args=('000651', '2020-01-01', '2023-12-31'))
+    # thread1 = threading.Thread(target=comment_thread_date, args=('000333', '2020-01-01', '2023-12-31'))
+    # thread2 = threading.Thread(target=comment_thread_date, args=('000729', '2020-01-01', '2023-12-31'))
 
+    # 中断之后重新通过_id接着爬取
     # thread1 = threading.Thread(target=comment_thread_id, args=('000651', 384942, 411959))
     # thread2 = threading.Thread(target=comment_thread_id, args=('000651', 62929, 321047))
 
