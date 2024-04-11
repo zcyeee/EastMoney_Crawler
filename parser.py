c97a16f4e0576d0532846e5cb159dd1375535adf
Chenyang Zhang <61002783+zcyeee@users.noreply.github.com>
2024-04-11 19:54:43 +0800

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from datetime import datetime
+from selenium import webdriver
 
 
 class PostParser(object):
@@ -29,6 +30,22 @@ class PostParser(object):
         url_element = html.find_element(By.CSS_SELECTOR, 'td:nth-child(3) > div > a')
         return url_element.get_attribute('href')
 
+    def get_post_year(self, html):
+        driver = webdriver.Chrome()
+        driver.get(self.parse_post_url(html))
+        date_str = driver.find_element(By.CSS_SELECTOR, 'div.newsauthor > div.author-info.cl > div.time').text
+        self.year = int(date_str[:4])
+        driver.quit()
+
+    @staticmethod
+    def judge_post_date(html):  # eastmoney has several fucking inaccurate display dates
+        try:
+            judge_element = html.find_element(By.CSS_SELECTOR, 'td:nth-child(3) > div > span')
+            if judge_element.text == '问董秘':  # is not None
+                return False
+        except:
+            return True
+
     def parse_post_date(self, html):
         time_element = html.find_element(By.CSS_SELECTOR, 'div.update.pub_time')
         time_str = time_element.text
@@ -39,18 +56,12 @@ class PostParser(object):
                 self.year -= 1
             self.month = month
 
+        if self.id == 1:  # initiate the post year (a little intricate)
+            self.get_post_year(html)
+
         date = f'{self.year}-{month:02d}-{day:02d}'
         time = time_str.split(' ')[1]
         return date, time
-
-    @staticmethod
-    def judge_post_date(html):  # eastmoney has several fucking inaccurate display dates
-        try:
-            judge_element = html.find_element(By.CSS_SELECTOR, 'td:nth-child(3) > div > span')
-            if judge_element.text == '问董秘':  # is not None
-                return False
-        except:
-            return True
 
     def parse_post_info(self, html):
         self.id += 1
